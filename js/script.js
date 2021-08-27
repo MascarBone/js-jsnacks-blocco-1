a27285aa7b05cb5a42b404c7c6510b2efe34501b
MascarBone <simone.bonelli@gmail.com>
2021-08-27 17:22:24 +0200

    Sncak 8 Created

--- js/script.js
+++ js/script.js
@@ -200,3 +200,43 @@
 // }
 
 // console.log(somma);
+
+
+
+
+
+
+
+
+
+// ##### Snack N8 #####
+// Generatore di "nomi cognomi" casuali: prendendo una lista di nomi e 
+// una lista di cognomi, Gatsby vuole generare una falsa lista di 8 invitati.
+// Stampiamo la lista in modo ordinato in html come elementi <li> figli di un unico <ul>.
+
+let listaNomi = ["Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Roger", "Steve", "Thomas", "Tim", "Ty", "Victor", "Walter"];
+let listaCognomi = ["Anderson", "Ashwoon", "Aikin", "Bateman", "Bongard", "Bowers", "Boyd", "Cannon", "Cast", "Deitz", "Dewalt", "Ebner", "Frick", "Hancock", "Haworth", "Hesch", "Hoffman", "Kassing", "Knutson", "Lawless", "Lawicki", "Mccord", "McCormack", "Miller", "Myers", "Nugent", "Ortiz", "Orwig", "Ory", "Paiser", "Pak", "Pettigrew", "Quinn", "Quizoz", "Ramachandran", "Resnick", "Sagar", "Schickowski", "Schiebel", "Sellon", "Severson", "Shaffer", "Solberg", "Soloman", "Sonderling", "Soukup", "Soulis", "Stahl", "Sweeney", "Tandy", "Trebil", "Trusela", "Trussel", "Turco", "Uddin", "Uflan", "Ulrich", "Upson", "Vader", "Vail", "Valente", "Van Zandt", "Vanderpoel", "Ventotla", "Vogal", "Wagle", "Wagner", "Wakefield", "Weinstein", "Weiss", "Woo", "Yang", "Yates", "Yocum", "Zeaser", "Zeller", "Ziegler", "Bauer", "Baxster", "Casal", "Cataldi", "Caswell", "Celedon", "Chambers", "Chapman", "Christensen", "Darnell", "Davidson", "Davis", "DeLorenzo", "Dinkins", "Doran", "Dugelman", "Dugan", "Duffman", "Eastman", "Ferro", "Ferry", "Fletcher", "Fietzer", "Hylan", "Hydinger", "Illingsworth", "Ingram", "Irwin", "Jagtap", "Jenson", "Johnson", "Johnsen", "Jones", "Jurgenson", "Kalleg", "Kaskel", "Keller", "Leisinger", "LePage", "Lewis", "Linde", "Lulloff", "Maki", "Martin", "McGinnis", "Mills", "Moody", "Moore", "Napier", "Nelson", "Norquist", "Nuttle", "Olson", "Ostrander", "Reamer", "Reardon", "Reyes", "Rice", "Ripka", "Roberts", "Rogers", "Root", "Sandstrom", "Sawyer", "Schlicht", "Schmitt", "Schwager", "Schutz", "Schuster", "Tapia", "Thompson", "Tiernan", "Tisler"];
+
+let listaGatsby = [];
+let indiceNomi = 0;
+let indiceCognomi = 0;
+
+for (let i = 0; i < 8; i++)
+{
+    indiceNomi = Math.floor(Math.random() * listaNomi.length);
+    indiceCognomi = Math.floor(Math.random() * listaCognomi.length);
+
+    listaGatsby.push(listaCognomi[indiceCognomi] + " " + listaNomi[indiceNomi]);
+}
+
+listaGatsby.sort();
+
+console.log(listaGatsby);
+
+// Aggiunta di un <ul> per questo Snack
+document.getElementById('creazione').innerHTML = '<ul id="listaInvitati"></ul>';
+
+for (let j = 0; j < listaGatsby.length; j++)
+{
+    document.getElementById('listaInvitati').innerHTML += '<li>' + listaGatsby[j] + '</li>';
+}
