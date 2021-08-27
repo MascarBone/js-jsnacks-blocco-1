ad6cf0e14c9190dc27c160213b6a2d70f160aec9
MascarBone <simone.bonelli@gmail.com>
2021-08-27 17:39:55 +0200

    Snack 9 Created

--- js/script.js
+++ js/script.js
@@ -214,29 +214,115 @@
 // una lista di cognomi, Gatsby vuole generare una falsa lista di 8 invitati.
 // Stampiamo la lista in modo ordinato in html come elementi <li> figli di un unico <ul>.
 
-let listaNomi = ["Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Roger", "Steve", "Thomas", "Tim", "Ty", "Victor", "Walter"];
-let listaCognomi = ["Anderson", "Ashwoon", "Aikin", "Bateman", "Bongard", "Bowers", "Boyd", "Cannon", "Cast", "Deitz", "Dewalt", "Ebner", "Frick", "Hancock", "Haworth", "Hesch", "Hoffman", "Kassing", "Knutson", "Lawless", "Lawicki", "Mccord", "McCormack", "Miller", "Myers", "Nugent", "Ortiz", "Orwig", "Ory", "Paiser", "Pak", "Pettigrew", "Quinn", "Quizoz", "Ramachandran", "Resnick", "Sagar", "Schickowski", "Schiebel", "Sellon", "Severson", "Shaffer", "Solberg", "Soloman", "Sonderling", "Soukup", "Soulis", "Stahl", "Sweeney", "Tandy", "Trebil", "Trusela", "Trussel", "Turco", "Uddin", "Uflan", "Ulrich", "Upson", "Vader", "Vail", "Valente", "Van Zandt", "Vanderpoel", "Ventotla", "Vogal", "Wagle", "Wagner", "Wakefield", "Weinstein", "Weiss", "Woo", "Yang", "Yates", "Yocum", "Zeaser", "Zeller", "Ziegler", "Bauer", "Baxster", "Casal", "Cataldi", "Caswell", "Celedon", "Chambers", "Chapman", "Christensen", "Darnell", "Davidson", "Davis", "DeLorenzo", "Dinkins", "Doran", "Dugelman", "Dugan", "Duffman", "Eastman", "Ferro", "Ferry", "Fletcher", "Fietzer", "Hylan", "Hydinger", "Illingsworth", "Ingram", "Irwin", "Jagtap", "Jenson", "Johnson", "Johnsen", "Jones", "Jurgenson", "Kalleg", "Kaskel", "Keller", "Leisinger", "LePage", "Lewis", "Linde", "Lulloff", "Maki", "Martin", "McGinnis", "Mills", "Moody", "Moore", "Napier", "Nelson", "Norquist", "Nuttle", "Olson", "Ostrander", "Reamer", "Reardon", "Reyes", "Rice", "Ripka", "Roberts", "Rogers", "Root", "Sandstrom", "Sawyer", "Schlicht", "Schmitt", "Schwager", "Schutz", "Schuster", "Tapia", "Thompson", "Tiernan", "Tisler"];
+// let listaNomi = ["Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Roger", "Steve", "Thomas", "Tim", "Ty", "Victor", "Walter"];
+// let listaCognomi = ["Anderson", "Ashwoon", "Aikin", "Bateman", "Bongard", "Bowers", "Boyd", "Cannon", "Cast", "Deitz", "Dewalt", "Ebner", "Frick", "Hancock", "Haworth", "Hesch", "Hoffman", "Kassing", "Knutson", "Lawless", "Lawicki", "Mccord", "McCormack", "Miller", "Myers", "Nugent", "Ortiz", "Orwig", "Ory", "Paiser", "Pak", "Pettigrew", "Quinn", "Quizoz", "Ramachandran", "Resnick", "Sagar", "Schickowski", "Schiebel", "Sellon", "Severson", "Shaffer", "Solberg", "Soloman", "Sonderling", "Soukup", "Soulis", "Stahl", "Sweeney", "Tandy", "Trebil", "Trusela", "Trussel", "Turco", "Uddin", "Uflan", "Ulrich", "Upson", "Vader", "Vail", "Valente", "Van Zandt", "Vanderpoel", "Ventotla", "Vogal", "Wagle", "Wagner", "Wakefield", "Weinstein", "Weiss", "Woo", "Yang", "Yates", "Yocum", "Zeaser", "Zeller", "Ziegler", "Bauer", "Baxster", "Casal", "Cataldi", "Caswell", "Celedon", "Chambers", "Chapman", "Christensen", "Darnell", "Davidson", "Davis", "DeLorenzo", "Dinkins", "Doran", "Dugelman", "Dugan", "Duffman", "Eastman", "Ferro", "Ferry", "Fletcher", "Fietzer", "Hylan", "Hydinger", "Illingsworth", "Ingram", "Irwin", "Jagtap", "Jenson", "Johnson", "Johnsen", "Jones", "Jurgenson", "Kalleg", "Kaskel", "Keller", "Leisinger", "LePage", "Lewis", "Linde", "Lulloff", "Maki", "Martin", "McGinnis", "Mills", "Moody", "Moore", "Napier", "Nelson", "Norquist", "Nuttle", "Olson", "Ostrander", "Reamer", "Reardon", "Reyes", "Rice", "Ripka", "Roberts", "Rogers", "Root", "Sandstrom", "Sawyer", "Schlicht", "Schmitt", "Schwager", "Schutz", "Schuster", "Tapia", "Thompson", "Tiernan", "Tisler"];
 
-let listaGatsby = [];
-let indiceNomi = 0;
-let indiceCognomi = 0;
+// let listaGatsby = [];
+// let indiceNomi = 0;
+// let indiceCognomi = 0;
 
-for (let i = 0; i < 8; i++)
+// for (let i = 0; i < 8; i++)
+// {
+//     indiceNomi = Math.floor(Math.random() * listaNomi.length);
+//     indiceCognomi = Math.floor(Math.random() * listaCognomi.length);
+
+//     listaGatsby.push(listaCognomi[indiceCognomi] + " " + listaNomi[indiceNomi]);
+// }
+
+// listaGatsby.sort();
+
+// console.log(listaGatsby);
+
+// // Aggiunta di un <ul> per questo Snack
+// document.getElementById('creazione').innerHTML = '<ul id="listaInvitati"></ul>';
+
+// for (let j = 0; j < listaGatsby.length; j++)
+// {
+//     document.getElementById('listaInvitati').innerHTML += '<li>' + listaGatsby[j] + '</li>';
+// }
+
+
+
+
+
+
+
+
+
+// ##### Snack N9 #####
+// A)
+// Crea due array che hanno un numero di elementi diversi (di almeno 6 elementi).
+// Aggiungi elementi casuali all'array che ha meno elementi, fino a quando ne avrà 
+// tanti quanti l'altro.
+
+// B)
+// Mostriamo in HTML le due liste in parallelo
+
+// C)
+// Inserisci una terza lista, che unisca le due precedenti e alle quali rimuovi gli elementi con indice multiplo di 3 e di 4
+
+let listaPrima = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let listaSeconda = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'l', 'm', 'n', 'o', 'p', 'q', 'r'];
+
+let listaTerza = [];
+
+// Aggiunta di una row e delle <ul> per questo Snack
+document.getElementById('creazione').innerHTML =    '<div class="row">' +
+                                                        '<div class="col-4">' +
+                                                            '<ul id="listaPrima"></ul>' +
+                                                        '</div>' +
+                                                        '<div class="col-4">' +
+                                                            '<ul id="listaSeconda"></ul>' +
+                                                        '</div>' +
+                                                        '<div class="col-4">' +
+                                                            '<ul id="listaTerza"></ul>' +
+                                                        '</div>' +
+                                                    '</div>';
+
+
+while (listaPrima.length != listaSeconda.length)
 {
-    indiceNomi = Math.floor(Math.random() * listaNomi.length);
-    indiceCognomi = Math.floor(Math.random() * listaCognomi.length);
-
-    listaGatsby.push(listaCognomi[indiceCognomi] + " " + listaNomi[indiceNomi]);
+    if (listaPrima.length < listaSeconda.length)
+    {
+        listaPrima.push(prompt("Inserisci un elemento"));
+        // console.log("Inserito un valore if" + index);
+    } else {
+        listaSeconda.push(prompt("Inserisci un elemento"));
+        // console.log("Inserito un valore else" + index);
+    }
 }
 
-listaGatsby.sort();
-
-console.log(listaGatsby);
-
-// Aggiunta di un <ul> per questo Snack
-document.getElementById('creazione').innerHTML = '<ul id="listaInvitati"></ul>';
-
-for (let j = 0; j < listaGatsby.length; j++)
+for (let i = 0; i < listaPrima.length; i++)
 {
-    document.getElementById('listaInvitati').innerHTML += '<li>' + listaGatsby[j] + '</li>';
+    document.getElementById('listaPrima').innerHTML += '<li>' + listaPrima[i] + '</li>';
+    document.getElementById('listaSeconda').innerHTML += '<li>' + listaSeconda[i] + '</li>';
+
+}
+console.log(listaPrima.length);
+console.log(listaSeconda.length);
+
+
+//
+// C)
+// 
+listaTerza = listaPrima.concat(listaSeconda);
+console.log(listaTerza);
+console.log(listaTerza.length);
+
+for (let j = listaTerza.length; j > 0; j--)
+{
+    if ( j != 0 && (j%3 == 0 || j%4 == 0) )
+    {
+        listaTerza.splice(j,1);
+        console.log('rimosso' + j);
+    }
+}
+
+console.log(listaTerza);
+console.log(listaTerza.length);
+
+for (let z = 0; z < listaTerza.length; z++)
+{
+    document.getElementById('listaTerza').innerHTML += '<li>' + listaTerza[z] + '</li>';
+
 }
